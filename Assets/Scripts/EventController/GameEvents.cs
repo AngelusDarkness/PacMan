a82560ca91680be956553a05229e03741240630f
Pablo Navajas <pablo.navajas@3ogs.com.ar>
2019-11-06 19:26:43 -0300

    Added Art placeholder and EventController script

---
+++ Assets/Scripts/EventController/GameEvents.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Events{
+    public class GameEvent  { }
+
+
+    public class SpeedChangeEvent : GameEvent
+    {
+        public float speed = 0;
+    }
+    
+    public class JumpEvent : GameEvent { }
+    
+}
+
+
